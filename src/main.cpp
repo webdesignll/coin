60d2c4859fec6838a260e68b6f7c3cd7ab90422d
Matt Corallo <coingenbot@bluematt.me>
2014-01-16 17:12:57 +0100

    Change block rate/subsidy/block value

--- src/main.cpp
+++ src/main.cpp
@@ -1074,7 +1074,7 @@ uint256 static GetOrphanRoot(const CBlockHeader* pblock)
 
 int64 static GetBlockValue(int nHeight, int64 nFees)
 {
-    int64 nSubsidy = 50 * COIN;
+    int64 nSubsidy = 200 * COIN;
 
     // Subsidy is cut in half every 210000 blocks, which will occur approximately every 4 years
     nSubsidy >>= (nHeight / 210000);
